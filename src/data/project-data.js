c99863a468de63ae4e987acc37b471e985ddb2b4
lahirulakmal23 <lahirulakmal893@gmail.com>
2026-01-24 16:24:50 +0530

    feat: add project data for Smart Crowd Management System and E-Commerce Web App

---
+++ src/data/project-data.js
@@ -0,0 +1,26 @@
+import project1 from "../assets/images/project01.png";
+import project2 from "../assets/images/project-coffe.png";
+
+
+export const projects = [
+  {
+    id: 1,
+    title: "Smart Crowd Management System",
+    image: project1,
+    description:
+      "Proud to present CrowdFlow, a full-stack web application built to manage large-scale events through real-time monitoring, role-based dashboards, and scalable system design.",
+    technologies: ["React", "Express", "Node.js", "MongoDB", "Tailwind CSS", "JWT", "webSockets", "chart.js"],
+    demoLink: "https://lnkd.in/gerqNjwj",
+    githubLink: "https://github.com/Haritha-Pawan/Crowd-Management-.git",
+  },
+  {
+    id: 2,
+    title: "E-Commerce Web App",
+    image: project2,
+    description:
+      "A full-featured e-commerce platform with product listing, cart, and checkout.",
+    technologies: ["React", "Context API", "CSS"],
+    demoLink: "https://your-ecommerce-demo.com",
+    githubLink: "https://github.com/yourusername/ecommerce-app",
+  },
+];
